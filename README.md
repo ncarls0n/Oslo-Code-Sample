d146c7fba6e496c47884054ee7777c219915c610
Nathan Carlson <ncarlson1997@gmail.com>
2025-12-02 01:58:15 -0500

    readme last updates

--- README.md
+++ README.md
@@ -4,15 +4,16 @@ This repository contains a sample of some of the codes I have worked on recently
 The codes in this repository are representative of a few proprietary codes that we have been using at CITA. I explain my contributions to each code in the following sections. We plan to make public versions of some of these codes in the next 6 months, but this work has not been completed yet, so I've done my best to explain the code structure of each subdirectory.
 
 ## Peak Patch
-
 In the directory `Oslo-Code-Sample/pp/` contains a subset of the [Peak Patch](https://mocks.cita.utoronto.ca/index.php/Peak_Patch_and_WebSky#The_Peak_Patch_simulations) dark matter halo code. This Fortran-based code was first introduced by [Bond and Meyers in 1996](https://ui.adsabs.harvard.edu/abs/1996ApJS..103....1B/abstract) and updated for massively parallel computing by [Stein et al., 2018](https://arxiv.org/abs/1810.07727). 
 
 My primary contributions to the Peak Patch simulations have been numerous coding updates to clean up the code and improve efficiency, as well as developing the novel primordial non-Gaussianity (PNG) models. Code efficiency updates are a bit challenging to isolate, as I've made many changes all through the source code `Oslo-Code-Sample/pp/src/` over the last few years, but there is a reasonable correlation that anywhere there are comments, I have made changes because most of the comments in this code base were added by me in an effort to imporve readability. The PNG models I have added can be found in `Oslo-Code-Sample/pp/src/modules/RandomField/RandomField.f90`, lines 290-525 outline each of the PNG models. These then reference several functions and subroutines that I added such as `zetang_sli`, `T_interp`, `chi_to_zeta`. Peak Patch is highly modular, so any updates ultimately result in cascading changes. In particular, PNG model updates have required additional updates to `Oslo-Code-Sample/pp/src/modules/RandomField/gaussian_field.f90`, `Oslo-Code-Sample/pp/src/modules/RandomField/pktable.f90` and many other scripts.
 
-## Peak Patch post-processing pipeline in python
+I also manage the Peak Patch and WebSky data access page, which includes readmes explaining how Peak Patch and WebSky work: [see this link](https://mocks.cita.utoronto.ca/index.php/Peak_Patch_and_WebSky).
 
-I have also produced a large Python class for post-processing Peak Patch and WebSky data, `Oslo-Code-Sample/peakpatchtools/peakpatchtools.py` which was made wholly by me. With this code, Peak Patch simulation results can be loaded into python object of class `PeakPatch`, from here many manipulations can be done. I produced a Jupyter notebook for an undergraduate summer student `Oslo-Code-Sample/JupyterNotebooks/example.ipynb` which provides a nice overview of how `peakpatchtools.py` works. 
+## Peak Patch post-processing pipeline in python
+I have also produced a large Python class for post-processing Peak Patch and WebSky data, `Oslo-Code-Sample/peakpatchtools/peakpatchtools.py` which was made wholly by me. With this code, Peak Patch simulation results can be loaded into python object of class `PeakPatch`, from here many manipulations can be done. I produced a Jupyter notebook for an undergraduate summer student `Oslo-Code-Sample/JupyterNotebooks/example.ipynb` which provides a nice overview of how `peakpatchtools.py` works.
+
+I've also included the Jupyter notebook for generating figures for my latest paper [2510.18312](https://arxiv.org/abs/2510.18312), `Oslo-Code-Sample/JupyterNotebooks/Paper 1.ipynb` as an additional example of the `peakpatchtools.py`.
 
 ## Fortran code to measure the powerspectrum from a large discretized field
-
-Another cleaner example of a code wholly written by me is a script to measure the power spectrum from a large discretized field in parallel on our HPC clusters. 
+Another cleaner example of a code wholly written by me is a script to measure the power spectrum from a large discretized field in parallel on our HPC clusters. The Fortran script is `Oslo-Code-Sample/get_powers_pectrum/get_power_omp.f90`, which is run using the function `get_power_spectrum()` in `peakpatchtools.py` (which I've explained in the previous section).
---
+++ get_powers_pectrum/get_power_omp.f90
@@ -0,0 +1,140 @@
+program get_powerspectrum
+    use, intrinsic :: iso_c_binding
+    use omp_lib
+    implicit none
+    include 'fftw3.f03'
+
+    ! Parameter declarations
+    real, parameter :: pi=4.*atan(1.)
+
+    ! Variable declarations
+    character(len=1024) field_file, out_file
+    integer n,i,j,k,ii,jj,kk,l,nbuff,nyquist,klen
+    integer(8) file_size
+    real s_box, dk, mode, weights(2)
+    real, allocatable :: kbins(:), P_k(:), P_0(:)
+
+    ! FFTW variable delcarations
+    real(c_float),            pointer :: f_x(:,:,:)
+    complex(c_float_complex), pointer :: f_k(:,:,:)
+    type(c_ptr)                       :: f_ptr, FT_plan, iFT_plan
+    integer(c_size_t) fft_size
+    integer(c_int)    nfft, nthreads
+
+    ! OpenMP variables
+    integer ierr
+    real start, finish
+
+    ! Read at command line
+    read(*,*) field_file ! file name for the 3D field
+    read(*,*) nbuff      ! buffer thickness (lattice units)
+    read(*,*) s_box      ! side length of the 3D field (Mpc)
+
+    ! Read file size in bytes, determine resolution n^3 of the 3D field
+    inquire(file=trim(field_file), size=file_size)
+    if(file_size==-1) then
+        write(*,*) 'Size of file '//trim(field_file)// &
+            ' could not be read. Exiting.'; stop
+    else
+        n = (file_size/4)**(1./3.)
+        if(4*n**3/=file_size) then
+            write(*,*) 'File must have n^3 elements where n is an integer. Exiting.'
+            stop
+        endif
+    endif
+
+    ! Allocate parallel FFTW arrays and setup pointers to C arrays
+    nfft     = n
+    fft_size = 2*(nfft/2+1)*nfft**2
+    f_ptr    = fftwf_alloc_real(fft_size)
+    call c_f_pointer( f_ptr, f_x, [2*(nfft/2+1),nfft,nfft] )
+    call c_f_pointer( f_ptr, f_k, [   nfft/2+1 ,nfft,nfft] )
+
+    ! Initialize parallel FFTW plans for single-precision 3D discrete Fourier transforms
+    call cpu_time(start)
+    ierr = fftwf_init_threads()
+    nthreads = omp_get_max_threads()
+    call fftwf_plan_with_nthreads( nthreads )
+    FT_plan  = fftwf_plan_dft_r2c_3d(nfft,nfft,nfft,f_x,f_k,fftw_estimate)
+    iFT_plan = fftwf_plan_dft_c2r_3d(nfft,nfft,nfft,f_k,f_x,fftw_estimate)
+    call cpu_time(finish)
+    write(*,*)
+    write(*,'(a25,es10.4e2,a8)') 'time to make FFT plans: ', finish-start, ' seconds'
+
+    call cpu_time(start)
+    ! Read real-space feild into FFTW array variable
+    open(unit=99,file=field_file,access='stream')
+    read(99,pos=1) (((f_x(i,j,k),i=1,nfft),j=1,nfft),k=1,nfft)
+    close(99)
+    call cpu_time(finish)
+    write(*,'(a24,es10.4e2,a8)') 'time to read 3D field: ', finish-start, ' seconds'
+
+    call cpu_time(start)
+    ! Calculate 3D DFT of field F(f_x)=f_k
+    call fftwf_execute_dft_r2c(FT_plan, f_x, f_k)
+    call cpu_time(finish)
+    write(*,'(a22,es10.4e2,a8)') 'time to execute FFT: ', finish-start, ' seconds'
+
+    ! Field variables
+    dk      = 2.*pi/s_box ! Fourier space resolution (Mpc^-1)
+    nyquist = n/2+1       ! the Nyquist number, an integer
+
+    ! Distance (lattice units) to furthest cell when you fold at Nyquist
+    klen = sqrt(3.)*nyquist
+
+    ! Allocate wavenumber and power spectrum arrays
+    allocate(kbins(klen))
+    allocate(P_k(klen))
+    allocate(P_0(klen))
+
+    ! Assign wavenumber bins
+    kbins(1:klen) = (/ (i*dk, i=0,klen-1) /)
+
+    ! Calculate power spectrum from Fourier space field f_k
+    call cpu_time(start)
+!$OMP PARALLEL DO PRIVATE(ii,jj,kk,mode,l,weights) FIRSTPRIVATE(n,nyquist) REDUCTION(+:P_k,P_0)
+    do i=1,n; if(i<=nyquist)then; ii=i-1; else; ii=n+1-i ;endif
+        do j=1,n; if(j<=nyquist)then; jj=j-1; else; jj=n+1-j ;endif
+            do k=1,n; if(k<=nyquist)then; kk=k-1; else; kk=n+1-k ;endif
+
+                mode       = sqrt(float(ii**2+jj**2+kk**2))+1
+                l          = floor(mode)
+                weights    = (1.-(/l-mode,l-mode+1./)**2.)**2.
+                P_k(l:l+1) = P_k(l:l+1) + weights * abs(f_k(ii+1,j,k))**2.
+                P_0(l:l+1) = P_0(l:l+1) + weights
+
+            enddo
+        enddo
+    enddo
+!$OMP END PARALLEL DO
+    call cpu_time(finish)
+    write(*,'(a35,es10.4e2,a8)') 'time to calculate power spectrum: ', &
+                                 (finish-start)/float(nthreads), ' seconds'
+
+    ! Divides out zero mode
+    do i=1,klen
+        if(P_0(i)/=0.) P_k(i) = P_k(i) / P_0(i)
+    enddo
+
+    ! Express modulus square of DFT Fourier modes P_k as samples of the of
+    ! the power spectrum P(k) of a continuous field. Because of our choice
+    ! of conventions, we have the relation P(k) = n^-3 (s_latt/2pi)^3 P_k
+    P_k = (s_box/(2.*pi*float(n*(n-2*nbuff))))**3. * P_k
+
+    ! Write out fundamental mode
+    write(*,*)
+    write(*,'(a44,es10.4e2,a8)') 'The 0th Fourier mode is P(k=0)=<|f(k=0)|^2>=',P_k(1),' Mpc^-1.'
+    write(*,*)
+
+    ! Save power spectrum in the form used by Peak Patch initial conditions
+    out_file = trim(field_file)//'_power.dat'
+    open(unit=42,file=out_file)
+    do i=2,klen
+        write(42,'(e10.5e2,a,e10.5e2)') kbins(i),' ',P_k(i)
+    enddo
+    close(42)
+    write(*,'(a23)') 'P(k>0) written to file:'
+    write(*,*) trim(out_file)
+    write(*,*)
+
+end program get_powerspectrum
